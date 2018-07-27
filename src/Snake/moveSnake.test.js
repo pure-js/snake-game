eeb419f80ab803abfcc14a24b22b61e23d4474b5
pure-js <3690368+pure-js@users.noreply.github.com>
2018-07-27 16:52:59 +0300

    rewrite test siute

--- src/Snake/moveSnake.test.js
+++ src/Snake/moveSnake.test.js
@@ -1,5 +1,24 @@
-import moveSnake from './moveSnake';
+import moveSnake, { calcHead } from './moveSnake';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(moveSnake(1, 2)).toBe(3);
+test('calc head position', () => {
+  expect(calcHead({x: 11, y: 12}, 'ArrowUp')).toEqual({x: 11, y: 11});
+  expect(calcHead({x: 11, y: 12}, 'ArrowRight')).toEqual({x: 12, y: 12});
+  expect(calcHead({x: 11, y: 12}, 'ArrowDown')).toEqual({x: 11, y: 13});
+  expect(calcHead({x: 11, y: 12}, 'ArrowLeft')).toEqual({x: 10, y: 12});
+});
+
+test('correct snake after turn', () => {
+  const snake = [
+    {x: 11, y: 12},
+    {x: 12, y: 12},
+    {x: 13, y: 12},
+  ];
+
+  const movedSnake = [
+    {x: 12, y: 12},
+    {x: 13, y: 12},
+    {x: 14, y: 12},
+  ];
+
+  expect(moveSnake(snake, 'ArrowRight')).toEqual(movedSnake);
 });
