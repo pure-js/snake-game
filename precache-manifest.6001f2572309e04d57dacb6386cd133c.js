08ff733c27b2f3e660585b16ce753c621dd7cb7f
pure-js <3690368+pure-js@users.noreply.github.com>
2019-01-07 13:49:08 +0300

    Updates

---
+++ precache-manifest.6001f2572309e04d57dacb6386cd133c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "351fc99354337a05b0be",
+    "url": "/snake-game/static/js/runtime~main.351fc993.js"
+  },
+  {
+    "revision": "5869cd16caec59f600ea",
+    "url": "/snake-game/static/js/main.5869cd16.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/snake-game/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "5869cd16caec59f600ea",
+    "url": "/snake-game/static/css/main.240fb798.chunk.css"
+  },
+  {
+    "revision": "0412d0d37f452417d07dbe57bfcea628",
+    "url": "/snake-game/index.html"
+  }
+];
